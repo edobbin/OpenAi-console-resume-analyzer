e4d0ea985b27c280442df1745f31db1873d8fdcc
edobbin <ethandobbin37@gmail.com>
2026-09-01 19:50:09 -0400

    started login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,25 @@
-import Navbar from "./components/layout/Navbar";
 import AnalyzerForm from "./components/analyzer/AnalyzerForm";
 import ResultPage from "./components/results/ResultPage";
 import { Route, Routes } from "react-router-dom";
 import LoadingPage from "./components/loading/loadingPage";
+import LoginPage from "./components/auth/LoginPage";
+import SignUpPage from "./components/auth/SignUpPage";
+import AppLayout from "./components/layout/AppLayout";
 
 function App() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
 
       <main className="page-container">
         <Routes>
-          <Route path="/" element={<AnalyzerForm />} />
-          <Route path="/analyzing" element={<LoadingPage />} />
-          <Route path="/results" element={<ResultPage />} />
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/" element={<SignUpPage />} />
+          <Route element={<AppLayout />}>
+            <Route path="/analyzer" element={<AnalyzerForm />} />
+            <Route path="/analyzing" element={<LoadingPage />} />
+            <Route path="/results" element={<ResultPage />} />
+          </Route>
         </Routes>
       </main>
     </>
